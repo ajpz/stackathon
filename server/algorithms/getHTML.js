9ce4eb68c2f9909f339df27b9de849e6b4330079
ajpz <perez.jonthan@gmail.com>
2016-01-21 19:17:30 -0500

    Promisifed http.request

---
+++ server/algorithms/getHTML.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var Promise = require('bluebird');
+
+var getHtml = function(url) {
+
+  var options = { host: url, path: '/', method: 'GET', headers: {Accept: "text/html"}};
+
+  return new Promise(function(resolve, reject) {
+
+    var request = http.request(options, function(response){
+      var html = '';
+      response.setEncoding('utf8');
+
+      response.on('data', function(chunk) {
+        html += chunk;
+      });
+
+      response.on('end', function(){
+        resolve(html);
+      });
+    });
+
+    request.on('error', function(err) {
+      console.error('problem occured: ', err.message);
+      reject(err);
+    });
+
+    request.end();
+  })
+}
+
+module.exports = getHtml;
