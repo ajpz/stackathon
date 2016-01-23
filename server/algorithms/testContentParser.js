7b096397b2034ce7523f069e2b2aed8f0e807f6e
ajpz <perez.jonthan@gmail.com>
2016-01-23 09:37:58 -0500

    Add prelim code for conentParsing -- no libraries leveraged

---
+++ server/algorithms/testContentParser.js
@@ -0,0 +1,13 @@
+var contentParser = require('./contentParser');
+var getHTML = require('./getHTML');
+
+console.log('INVOKED...........');
+
+getHTML('http://www.cnn.com')
+.then(function(html) {
+  console.log('GOT HTML')
+  return contentParser(html.html);
+})
+.then(function(contents) {
+  console.log('\n\nCONTENTS are : \n\n', contents);
+})
