a14ccf797cc65c2b45b392533d9584dcf1c335a8
jonatans <jonatan@jschumacher.com>
2016-01-23 10:09:58 -0500

    moved test files to subdir

--- server/algorithms/testLibraries/read.js
+++ server/algorithms/testLibraries/read.js
@@ -1,14 +1,14 @@
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
 var path = require('path');
-​
+
 var readDirRecursive = function (dirName) {
     return fs.readdirAsync(dirName)
         .map(function (fileOrDirInDirectory) {
             var fullPath = path.join(dirName, fileOrDirInDirectory);
             return fs.statAsync(fullPath)
-                .then(funcxtion (stat) {
-                    if (stat.isDirectory()) {
+            .then(function(stat){
+                if (stat.isDirectory()) {
                         return readDirRecursive(fullPath);
                     } else {
                         return fullPath;
@@ -16,7 +16,7 @@ var readDirRecursive = function (dirName) {
                 });
         });
 };
-​console.log(path.join(__dirname, '..'));
-readDirRecursive(path.join(__dirname, '..'));
-    .then(console.log)
-    .catch(console.error);
+console.log(path.join(__dirname, '..'));
+readDirRecursive(path.join(__dirname, '..'))
+.then(console.log)
+.catch(console.error);
