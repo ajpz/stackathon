10fd78b4ab6c7801d511a5f0a5878fe870316219
jonatans <jonatan@jschumacher.com>
2016-01-23 09:42:51 -0500

    crawl recursively through promise wonderland

--- server/algorithms/getHTML.js
+++ server/algorithms/getHTML.js
@@ -3,32 +3,34 @@ var Promise = require('bluebird');
 var urlParser = require('url').parse;
 
 var getHtml = function(href) {
-
+  console.log('beginning getHtml', href);
   var parsedUrl = urlParser(href),
       host = parsedUrl.host,
       path = parsedUrl.path || '/';
 
   if(!path) path = '/';
-
+  console.log(path);
   var options = { host: host, path: path, method: 'GET', headers: {Accept: "text/html"}};
 
   return new Promise(function(resolve, reject) {
-
+    console.log('requesting html');
     var request = http.request(options, function(response){
       var html = '';
       response.setEncoding('utf8');
 
       response.on('data', function(chunk) {
+        //console.log('getting data');
         html += chunk;
       });
 
       response.on('end', function(){
+        //console.log('end', html);
         resolve({ host: host, html: html });
       });
     });
 
     request.on('error', function(err) {
-      console.error('problem occured: ', err.message);
+      console.error('\n\n\nERROR!!!\n\n\nproblem occured: ', err.message);
       reject(err);
     });
 
